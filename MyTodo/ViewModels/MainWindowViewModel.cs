ad329259055de7dadb2097bd3b025ceec20a234f
Tobias Hoppenthaler <tobias.hoppenthaler@msg.group>
2017-12-05 10:00:48 +0100

    remove todo with trigger and binding selected item on mouseover

--- MyTodo/ViewModels/MainWindowViewModel.cs
+++ MyTodo/ViewModels/MainWindowViewModel.cs
@@ -16,7 +16,7 @@ namespace MyTodo.ViewModels
 
         public ObservableCollection<Todo> Todos { get; set; } = new ObservableCollection<Todo>();
 
-
+        public Todo SelectedTodo { get; set; }  
         
         public void AddTodo(object sender, RoutedEventArgs eventArgs)
         {
@@ -29,8 +29,7 @@ namespace MyTodo.ViewModels
 
         public void RemoveTodo(object sender, RoutedEventArgs eventArgs)
         {
-            Todo selectedTodo = (Todo)((Button) sender).Tag;
-            Todos.Remove(selectedTodo);
+            Todos.Remove(SelectedTodo);
         }
 
        
